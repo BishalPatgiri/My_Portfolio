748d98bcb2dedee60241d1b4b095af4a535132f2
BishalPatgiri <bishalpatgiri67@gmail.com>
2022-09-12 22:24:12 +0530

    done some edits

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -4,6 +4,7 @@ import project2 from "../Images/p2.png"
 import project3 from "../Images/p3.png"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faCss3, faGithub, faHtml5, faReact} from "@fortawesome/free-brands-svg-icons"
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 export const Projects = () => {
   return (
@@ -14,6 +15,24 @@ export const Projects = () => {
       </h1>
       </div>
       <div id='projectRes' style={{}}>
+      <div className='projectDiv'>
+          <img className='projectImp' src={project3} alt="" />
+          <p style={{fontSize:"25px",fontStyle:"italic"}}>Naturesbascket.com Clone</p>
+          <p>As the name, this website is all about the neturel things that we have been eating in our day to day life. It provides all the varities of food and bevarages available in the marget. I have designed this website by my own withn a week by implementing most of the functionalities of the website.</p>
+          <div style={{marginBottom:"20px"}}>
+          <FontAwesomeIcon style={{color:"white",fontSize:"48px",marginRight:"10px"}} icon={faGithub}></FontAwesomeIcon> 
+          <FontAwesomeIcon style={{color:"tomato",fontSize:"48px",marginRight:"10px"}} icon={faHtml5}></FontAwesomeIcon>
+          <FontAwesomeIcon style={{color:"yellowgreen",fontSize:"48px",marginRight:"10px"}} icon={faCss3}></FontAwesomeIcon>
+          <FontAwesomeIcon style={{color:"purple",fontSize:"48px",marginRight:"10px"}} icon={faReact}></FontAwesomeIcon>
+          </div>
+          <div style={{display:"flex",justifyContent:"space-between"}}>
+          <a target={"blank"} href="https://github.com/BishalPatgiri/Naturesbasket-Clone" style={{textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>
+            <FontAwesomeIcon style={{color:"white",fontSize:"25px",marginRight:"10px"}} icon={faGithub}></FontAwesomeIcon> GitHub
+            </a>
+            <a target={"blank"} href="https://beautiful-empanada-207bf8.netlify.app/" style={{textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>Live
+            <FontAwesomeIcon style={{marginLeft:"10px",fontSize:"22px"}} icon={faArrowUpRightFromSquare}></FontAwesomeIcon></a>
+          </div>
+        </div>
         <div className='projectDiv'>
           <img className='projectImp' src={project1} alt="" />
           <p style={{fontSize:"25px",fontStyle:"italic"}}>Sephora.nnnow.com Clone</p>
@@ -28,7 +47,7 @@ export const Projects = () => {
             <a target={"blank"} href="https://github.com/BishalPatgiri/SEPHORA-E-commerce-Website" style={{textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>
             <FontAwesomeIcon style={{color:"white",fontSize:"25px",marginRight:"10px"}} icon={faGithub}></FontAwesomeIcon> GitHub
             </a>
-            <a target={"blank"} href="https://storied-marzipan-b1c3f6.netlify.app/" style={{textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>Delployed</a>
+            <a target={"blank"} href="https://storied-marzipan-b1c3f6.netlify.app/" style={{textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>Live<FontAwesomeIcon style={{marginLeft:"10px",fontSize:"22px"}} icon={faArrowUpRightFromSquare}></FontAwesomeIcon></a>
           </div>
         </div>
         <div className='projectDiv'>
@@ -45,24 +64,8 @@ export const Projects = () => {
           <a target={"blank"} href="https://github.com/BishalPatgiri/Geekbuying-E-Commerce" style={{textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>
             <FontAwesomeIcon style={{color:"white",fontSize:"25px",marginRight:"10px"}} icon={faGithub}></FontAwesomeIcon> GitHub
             </a>
-            <a target={"blank"} href="https://endearing-lebkuchen-37c83d.netlify.app/" style={{marginTop:"4px",textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>Delployed</a>
-          </div>
-        </div>
-        <div className='projectDiv'>
-          <img className='projectImp' src={project3} alt="" />
-          <p style={{fontSize:"25px",fontStyle:"italic"}}>Naturesbascket.com Clone</p>
-          <p>As the name, this website is all about the neturel things that we have been eating in our day to day life. It provides all the varities of food and bevarages available in the marget. I have designed this website by my own withn a week by implementing most of the functionalities of the website.</p>
-          <div style={{marginBottom:"20px"}}>
-          <FontAwesomeIcon style={{color:"white",fontSize:"48px",marginRight:"10px"}} icon={faGithub}></FontAwesomeIcon> 
-          <FontAwesomeIcon style={{color:"tomato",fontSize:"48px",marginRight:"10px"}} icon={faHtml5}></FontAwesomeIcon>
-          <FontAwesomeIcon style={{color:"yellowgreen",fontSize:"48px",marginRight:"10px"}} icon={faCss3}></FontAwesomeIcon>
-          <FontAwesomeIcon style={{color:"purple",fontSize:"48px",marginRight:"10px"}} icon={faReact}></FontAwesomeIcon>
-          </div>
-          <div style={{display:"flex",justifyContent:"space-between"}}>
-          <a target={"blank"} href="https://github.com/BishalPatgiri/Naturesbasket-Clone" style={{textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>
-            <FontAwesomeIcon style={{color:"white",fontSize:"25px",marginRight:"10px"}} icon={faGithub}></FontAwesomeIcon> GitHub
-            </a>
-            <a target={"blank"} href="https://beautiful-empanada-207bf8.netlify.app/" style={{marginTop:"4px",textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>Delployed</a>
+            <a target={"blank"} href="https://endearing-lebkuchen-37c83d.netlify.app/" style={{textDecoration:"none",border:"1px solid white",color:"white",padding:"6px",borderRadius:"8px"}}>Live
+            <FontAwesomeIcon style={{marginLeft:"10px",fontSize:"22px"}} icon={faArrowUpRightFromSquare} /></a>
           </div>
         </div>
       </div>
